b168cc597e1d7c46c1927633fa92cd9e39d389b8
Neel Nadkarni <nsnadkarni@cps.edu>
2023-10-24 10:32:15 -0500

    finished

--- a4.py
+++ a4.py
@@ -1,6 +1,26 @@
 # NOTE: Until you fill in the TTTBoard class mypy is going to give you multiple errors
 # talking about unimplemented class attributes, don't worry about this as you're working
 
+def check_row(pos: int, player: str, board: list) -> bool:
+    if board[pos] == player and board[pos + 1] == player and board[pos + 2] == player:
+        return True
+    return False
+
+
+def check_col(pos: int, player: str, board: list) -> bool:
+    if board[pos] == player and board[pos + 3] == player and board[pos + 6] == player:
+        return True
+    return False
+
+def check_diag(pos: int, player: str, board: list, a: bool) -> bool:
+    if(a == True):
+        if board[pos] == player and board[pos + 4] == player and board[pos + 8]:
+            return True
+        return False
+    else:
+        if board[pos] == player and board[pos + 2] == player and board[pos + 4]:
+            return True
+        return False
 
 class TTTBoard:
     """A tic tac toe board
@@ -9,6 +29,58 @@ class TTTBoard:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
+
+    def __init__(self) -> None:
+        self.board = ['*'] * 9
+
+
+    def __str__(self) -> str:
+        return f"{self.board[0]} {self.board[1]} {self.board[2]}\n{self.board[3]} {self.board[4]} {self.board[5]}\n{self.board[6]} {self.board[7]} {self.board[8]}"
+
+
+    def make_move(self, player: str, pos: int) -> bool:
+        if pos > 8 or pos < 0 or self.board[pos] != "*":
+            return False
+        self.board[pos] = player
+        return True
+
+
+    def has_won(self, player: str):
+        b = player
+        c = self.board
+        if check_col(0, b, c) or check_col(1, b, c) or check_col(2, b, c):
+            return True
+        if check_row(0, b, c) or check_row(3, b, c) or check_row(6, b, c):
+            return True
+        if check_diag(0, b, c, True) or check_diag(2, b, c, False):
+            return True
+        
+        return False 
+
+    def game_over(self):
+        b = "X"
+        c = self.board
+        if check_col(0, b, c) or check_col(1, b, c) or check_col(2, b, c):
+            return True
+        if check_row(0, b, c) or check_row(3, b, c) or check_row(6, b, c):
+            return True
+        if check_diag(0, b, c, True) or check_diag(2, b, c, False):
+            return True
+        b = "O"
+        c = self.board
+        if check_col(0, b, c) or check_col(1, b, c) or check_col(2, b, c):
+            return True
+        if check_row(0, b, c) or check_row(3, b, c) or check_row(6, b, c):
+            return True
+        if check_diag(0, b, c, True) or check_diag(2, b, c, False):
+            return True
+        for i in range(8):
+            if(self.board[i] == '*'):
+                break
+        return False
+
+    def clear(self):
+        self.board = ['*'] * 9
 
     pass
 
@@ -61,6 +133,7 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd)
     brd.make_move("X", 8)
     brd.make_move("O", 7)
 
--- notes.py
+++ notes.py
@@ -2,3 +2,19 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n = "", fc = "", a = 0, ff = "") -> None:
+        self.name = n
+        self.fur_color = fc
+        self.age = a
+        self.favorite_food = ff
+        self.fetch_count = 0
+
+    def __str__(self) -> str:
+        """Return a string representation of a dog"""
+        return f"{self.name}'s fur color is {self.fur_color}, {self.name}'s age is {self.age}, {self.name}'s favorite food is {self.favorite_food}, and {self.name}'s fetch count is {self.fetch_count}"
+    
+mydog = Dog("logan", "black", 7, "salmon")
+chrisdog = Dog("luna", "black and white", 6, "tortillas")
+
+print(chrisdog)
